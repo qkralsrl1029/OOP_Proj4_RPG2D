70cf461d34f80e0729f81f74248a05b3ef676acc
유호성 <h0song@nave.rcom>
2020-12-04 22:52:23 +0900

    commit
    draw monsters

--- Assets/2.Scripts/monsterGenerator.cs
+++ Assets/2.Scripts/monsterGenerator.cs
@@ -7,8 +7,8 @@ public class monsterGenerator : MonoBehaviour
     [SerializeField] MonsterController[] monsterPrefab = new MonsterController[4];
     List<MonsterController> monsters = new List<MonsterController>();
     [SerializeField] GameObject goal;
- 
-    [SerializeField] int[,] count = new int[3, 4] { { 2, 4 ,0, 0} , { 2,4,2,0},{2,4,2,1 } };
+
+    [SerializeField] int[] count = new int[4];
     
     [SerializeField] Transform[] monster_pos;
     [SerializeField] Transform goal_pos;
@@ -19,29 +19,31 @@ public class monsterGenerator : MonoBehaviour
        
         DontDestroyOnLoad(gameObject);
         //count[0,0] =2 goblin
-        for (int i = 0; i < count[0,0]; i++)
+        for (int i = 0; i < count[0]; i++)
         {
             MonsterController go = Instantiate(monsterPrefab[0], monster_pos[i].position, Quaternion.identity);
             monsters.Add(go);
         }
         //count[0,1] =4 skeleton
-        for (int i = 0; i < count[0,1]; i++)
+        for (int i = 0; i < count[1]; i++)
         {
-            MonsterController go = Instantiate(monsterPrefab[1], monster_pos[i + count[0,0]].position, Quaternion.identity);
+            MonsterController go = Instantiate(monsterPrefab[1], monster_pos[i + count[0]].position, Quaternion.identity);
             monsters.Add(go);
         }
         //count[0,2] = 0 mushroom
-        for (int i = 0; i < count[0, 2] ; i++)
+        for (int i = 0; i < count[2] ; i++)
         {
-            MonsterController go = Instantiate(monsterPrefab[2], monster_pos[i + count[0,0] + count[0,1]].position, Quaternion.identity);
+            MonsterController go = Instantiate(monsterPrefab[2], monster_pos[i + count[0] + count[1]].position, Quaternion.identity);
             monsters.Add(go);
         }
         //count[0,2] = 0 boss
-        for (int i = 0; i < count[0, 3]; i++)
+        for (int i = 0; i < count[3]; i++)
         {
-            MonsterController go = Instantiate(monsterPrefab[3], monster_pos[i + count[0, 0] + count[0, 1] + count[0,2]].position, Quaternion.identity);
+            MonsterController go = Instantiate(monsterPrefab[3], monster_pos[i + count[0] + count[1] + count[2]].position, Quaternion.identity);
             monsters.Add(go);
         }
+
+        //monster_pos 6번까지 사용
     }
     
     public void createMonster()
